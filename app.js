2ab07f71218d355ab8dca083ca538620bcf2238b
Sri-sh-ti <72740967+Sri-sh-ti@users.noreply.github.com>
2021-02-28 17:41:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -54,7 +54,38 @@ function startRecording() {
 		
 		
 		//update the format 
-		document.getElementById("formats").innerHTML
+		document.getElementById("formats").innerHTMLfunction downsampleBuffer(buffer, rate) {
+                if (rate == sampleRate) {
+                    return buffer;
+                }
+                if (rate > sampleRate) {
+                    throw "downsampling rate show be smaller than original sample rate";
+                }
+                var sampleRateRatio = sampleRate / rate;
+                var newLength = Math.round(buffer.length / sampleRateRatio);
+                var result = new Float32Array(newLength);
+                var offsetResult = 0;
+                var offsetBuffer = 0;
+                while (offsetResult < result.length) {
+                    var nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio);
+                     // Use average value of skipped samples
+                    var accum = 0, count = 0;
+                    for (var i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
+                        accum += buffer[i];
+                        count++;
+                    }
+                    result[offsetResult] = accum / count;
+                    // Or you can simply get rid of the skipped samples:
+                    // result[offsetResult] = buffer[nextOffsetBuffer];
+                    offsetResult++;
+                    offsetBuffer = nextOffsetBuffer;
+                }
+                return result;
+            }
+		
+	var downsampledBuffer = downsampleBuffer(interleaved, targetRate);
+        var dataview = encodeWAV(downsampledBuffer);	
+		
 
 		/*  assign to gumStream for later use  */
 		gumStream = stream;
